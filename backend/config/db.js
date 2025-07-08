56c32446cb6322de9b25cf749b9ca0ba508767f4
madgepereira020701 <madgepereira020701@gmail.com>
2025-07-08 11:08:46 +0530

    Modified

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,8 @@ mongoose.connection.on("error", (err) => {
   console.log("Mongoose not found");
 });
 
+mongoURI =
+  "mongodb+srv://projectmanagement:tcejorp@projectmanagement.uaexwyv.mongodb.net/?retryWrites=true&w=majority&appName=ProjectManagement";
 const connectDB = async () => {
   try {
     await mongoose.connect(mongoURI, {
